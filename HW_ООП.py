00d08ee4bf14cec837bea9aeb7c5b4e7bfe02511
ManGitH <man.martynenko@yandex.ru>
2022-04-04 00:07:54 +0300

    enhd

--- HW_ООП.py
+++ HW_ООП.py
@@ -1,3 +1,14 @@
+from typing import Any, Dict
+
+
+def avg_grd(a, b):
+    avg = {}
+    for l, g in b.items():
+        sum_ag = sum(g) / len(g)
+        avg[l] = sum_ag
+    res = f'''{avg}'''
+    return res
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -17,7 +28,7 @@ class Student:
             return 'Ошибка'
 
     def __str__(self):
-        res = f'''Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за домашнее задание: 
+        res = f'''Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за домашнее задание: {avg_grd(self, self.grades)}
         \nКурсы в процессе изучения: {",".join(self.courses_in_progress)}\nзавершенные курсы: {",".join(self.finished_courses)}'''
         return res
 
@@ -33,18 +44,14 @@ class Mentor:
         return res
 
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
-    def avggrd(self):
-        for l, g in self.grades.items():
-            sum_ag = sum(g) / len(g)
-        return sum_ag
-    
     def __str__(self):
-        res = f'''Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за лекции: {print(self.avggrd)}'''
+        res = f'''Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за лекции: {avg_grd(self, self.grades)}'''
         return res
 
 
@@ -94,9 +101,9 @@ fc = f'''Курсы которые закончили студенты:
                             {student_2.name}-{student_2.finished_courses}, 
                             {student_3.name}-{student_3.finished_courses},
                             {best_student.name}-{best_student.finished_courses}'''
-print(sn)
-print(pc)
-print(fc)
+# print(sn)
+# print(pc)
+# print(fc)
 
 mentor_1 = Mentor('Oleg', 'Sherbakov')
 mentor_2 = Mentor('Andrey', 'Martynenko')
@@ -107,7 +114,7 @@ cool_mentor = Mentor('Some', 'Buddy')
 # mentor_3.courses_attached += ['SQL']
 # cool_mentor.courses_attached += ['Python']
 mn = f'''Имена менторов: {mentor_1.name}, {mentor_2.name}, {mentor_3.name}, {cool_mentor.name}'''
-print(mn)
+# print(mn)
 
 lecturer_1 = Lecturer('Aleksandr', 'Knips')
 lecturer_2 = Lecturer('Slava', 'Nazarevich')
@@ -115,10 +122,10 @@ lecturer_1.courses_attached += ['PHP']
 lecturer_1.courses_attached += ['SQL']
 lecturer_2.courses_attached += ['JS']
 lecturer_2.courses_attached += ['Python']
-student_1.rate_lecturer = (lecturer_1, 'PHP', 10)
-student_2.rate_lecturer = (lecturer_1, 'PHP', 6)
-student_3.rate_lecturer = (lecturer_2, 'JS', 9)
-best_student.rate_lecturer = (lecturer_2, 'JS', 5)
+student_1.rate_lecturer(lecturer_1, 'PHP', 10)
+student_2.rate_lecturer(lecturer_1, 'PHP', 6)
+student_3.rate_lecturer(lecturer_2, 'JS', 9)
+best_student.rate_lecturer(lecturer_1, 'JS', 5)
 ln = f'''Имена лекторов: {lecturer_1}, {lecturer_2}'''
 lc = f'''Курсы которые ведут лекторы: 
                             {lecturer_1.name}-{lecturer_1.courses_attached}, 
@@ -127,13 +134,15 @@ lc = f'''Курсы которые ведут лекторы:
 
 # print(ln)
 # print(lc)
-print(lecturer_1)
+student_1.rate_lecturer(lecturer_1, 'JS', 5)
+print(lecturer_1.grades)
+print(lecturer_2)
 
 reviewer_1 = Reviewer('Artem', 'Bibanin')
 reviewer_2 = Reviewer('Some', 'Buddy')
 reviewer_1.rate_hw(student_1, 'PHP', 8)
 reviewer_2.rate_hw(student_2, 'JS', 6)
-print(reviewer_1.name, reviewer_2.name)
+# print(reviewer_1.name, reviewer_2.name)
 
 print(student_1)
 
