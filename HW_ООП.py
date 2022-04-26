ff3cc4f2ef30b627621db26846f03b8a7065732d
ManGitH <man.martynenko@yandex.ru>
2022-04-26 14:31:27 +0300

    modif

--- HW_ООП.py
+++ HW_ООП.py
@@ -1,6 +1,3 @@
-from typing import Any, Dict
-
-
 def avg_grd(a, b):
     avg = {}
     for l, g in b.items():
@@ -8,6 +5,7 @@ def avg_grd(a, b):
         avg[l] = sum_ag
     res = f'''{avg}'''
     return res
+
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -19,7 +17,8 @@ class Student:
         self.grades = {}
 
     def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -29,7 +28,7 @@ class Student:
 
     def __str__(self):
         res = f'''Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за домашнее задание: {avg_grd(self, self.grades)}
-        \nКурсы в процессе изучения: {",".join(self.courses_in_progress)}\nзавершенные курсы: {",".join(self.finished_courses)}'''
+        \nКурсы в процессе изучения: {",".join(self.courses_in_progress)}\nЗавершенные курсы: {",".join(self.finished_courses)}'''
         return res
 
 
@@ -42,7 +41,6 @@ class Mentor:
     def __str__(self):
         res = f'''Имя:{self.name}\nФамилия:{self.surname}'''
         return res
-
 
 
 class Lecturer(Mentor):
@@ -70,85 +68,44 @@ class Reviewer(Mentor):
         return res
 
 
-student_1 = Student('Sergey', 'Fetisov', 'm')
-student_2 = Student('Andrey', 'Miroshnikov', 'm')
-student_3 = Student('Aleksandr', 'Plotnikov', 'm')
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-student_1.courses_in_progress += ['PHP']
-student_1.courses_in_progress += ['JS']
-student_2.courses_in_progress += ['JS']
-student_2.courses_in_progress += ['SQL']
-student_3.courses_in_progress += ['SQL']
-student_3.courses_in_progress += ['Python']
-best_student.courses_in_progress += ['Python']
-best_student.courses_in_progress += ['PHP']
-student_1.finished_courses += ['Python']
-student_1.finished_courses += ['SQL']
-student_2.finished_courses += ['PHP']
-student_2.finished_courses += ['Python']
-student_3.finished_courses += ['JS']
-student_3.finished_courses += ['PHO']
-best_student.finished_courses += ['JS']
-best_student.finished_courses += ['SQL']
-sn = f'''Имена студентов: {student_1.name}, {student_2.name}, {student_3.name}, {best_student.name}'''
-pc = f'''Курсы на которые записаны студенты: 
-{student_1.name}-{student_1.courses_in_progress}, 
-{student_2.name}-{student_2.courses_in_progress}, 
-{student_3.name}-{student_3.courses_in_progress},
-{best_student.name}-{best_student.courses_in_progress}'''
-fc = f'''Курсы которые закончили студенты: 
-                            {student_1.name}-{student_1.finished_courses}, 
-                            {student_2.name}-{student_2.finished_courses}, 
-                            {student_3.name}-{student_3.finished_courses},
-                            {best_student.name}-{best_student.finished_courses}'''
-# print(sn)
-# print(pc)
-# print(fc)
+student1 = Student('Andrey', 'Martynenko', 'm')
+student1.courses_in_progress = ['Python', 'GIT']
+student1.finished_courses = ['PHP', 'SQL']
 
-mentor_1 = Mentor('Oleg', 'Sherbakov')
-mentor_2 = Mentor('Andrey', 'Martynenko')
-mentor_3 = Mentor('Uri', 'Sinatulin')
-cool_mentor = Mentor('Some', 'Buddy')
-# mentor_1.courses_attached += ['PHP']
-# mentor_2.courses_attached += ['JS']
-# mentor_3.courses_attached += ['SQL']
-# cool_mentor.courses_attached += ['Python']
-mn = f'''Имена менторов: {mentor_1.name}, {mentor_2.name}, {mentor_3.name}, {cool_mentor.name}'''
-# print(mn)
-
-lecturer_1 = Lecturer('Aleksandr', 'Knips')
-lecturer_2 = Lecturer('Slava', 'Nazarevich')
-lecturer_1.courses_attached += ['PHP']
-lecturer_1.courses_attached += ['SQL']
-lecturer_2.courses_attached += ['JS']
-lecturer_2.courses_attached += ['Python']
-student_1.rate_lecturer(lecturer_1, 'PHP', 10)
-student_2.rate_lecturer(lecturer_1, 'PHP', 6)
-student_3.rate_lecturer(lecturer_2, 'JS', 9)
-best_student.rate_lecturer(lecturer_1, 'JS', 5)
-ln = f'''Имена лекторов: {lecturer_1}, {lecturer_2}'''
-lc = f'''Курсы которые ведут лекторы: 
-                            {lecturer_1.name}-{lecturer_1.courses_attached}, 
-                            {lecturer_2.name}-{lecturer_2.courses_attached}'''
-# lg = f'''Оценки студентов лекторам: {lecturer_1.name}-{}, {lecturer_2.name}'''
-
-# print(ln)
-# print(lc)
-student_1.rate_lecturer(lecturer_1, 'JS', 5)
-print(lecturer_1.grades)
-print(lecturer_2)
-
-reviewer_1 = Reviewer('Artem', 'Bibanin')
-reviewer_2 = Reviewer('Some', 'Buddy')
-reviewer_1.rate_hw(student_1, 'PHP', 8)
-reviewer_2.rate_hw(student_2, 'JS', 6)
-# print(reviewer_1.name, reviewer_2.name)
-
-print(student_1)
+student2 = Student('Dasha', 'Yacenko', 'f')
+student2.courses_in_progress = ['PHP', 'SQL']
+student2.finished_courses = ['Python', 'GIT']
 
 
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
+mentor1 = Mentor('Oleg', 'Sherbakov')
+mentor1.courses_attached = ['Python', 'GIT', 'PHP', 'SQL']
+mentor2 = Mentor('Sergei', 'Fetisov')
+mentor2.courses_attached = ['Python', 'GIT', 'PHP', 'SQL']
+print(mentor1)
+print(mentor2)
 
-# print(best_student.grades)
+lector1 = Lecturer('Oleg', 'Sherbakov')
+lector1.courses_attached = ['Python', 'GIT', 'PHP', 'SQL']
+student1.rate_lecturer(lector1, 'PHP', 5)
+student1.rate_lecturer(lector1, 'GIT', 5)
+student1.rate_lecturer(lector1, 'Python', 5)
+student1.rate_lecturer(lector1, 'SQL', 5)
+student2.rate_lecturer(lector1, 'PHP', 9)
+student2.rate_lecturer(lector1, 'GIT', 8)
+student2.rate_lecturer(lector1, 'Python', 2)
+student2.rate_lecturer(lector1, 'SQL', 1)
+print(lector1)
+
+reviewer1 = Reviewer('Sergei', 'Fetisov')
+reviewer1.courses_attached = ['Python', 'GIT', 'PHP', 'SQL']
+reviewer1.rate_hw(student1,'PHP', 5)
+reviewer1.rate_hw(student1, 'GIT', 5)
+reviewer1.rate_hw(student1,'Python', 5)
+reviewer1.rate_hw(student1,'PSQL', 5)
+reviewer1.rate_hw(student2,'PHP', 4)
+reviewer1.rate_hw(student2, 'GIT', 3)
+reviewer1.rate_hw(student2,'Python', 2)
+reviewer1.rate_hw(student2,'PSQL', 1)
+print(reviewer1)
+print(student1)
+print(student2)
